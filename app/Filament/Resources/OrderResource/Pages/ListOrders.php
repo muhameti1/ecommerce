fc911cf3de9691fac4c749d501f797666ab98d14
Fatbardha Dragusha <fatbardha.dragusha123@gmail.com>
2024-07-07 19:34:03 +0200

    Filtrimi i Orders ne baze te statusit

--- app/Filament/Resources/OrderResource/Pages/ListOrders.php
+++ app/Filament/Resources/OrderResource/Pages/ListOrders.php
@@ -3,8 +3,10 @@
 namespace App\Filament\Resources\OrderResource\Pages;
 
 use App\Filament\Resources\OrderResource;
+use App\Filament\Resources\OrderResource\Widgets\OrderStats;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
+use Filament\Resources\Components\Tab;
 
 class ListOrders extends ListRecords
 {
@@ -16,4 +18,38 @@ class ListOrders extends ListRecords
             Actions\CreateAction::make(),
         ];
     }
-}
+
+     protected function getHeaderWidgets(): array
+     {
+
+           return [
+
+               OrderStats::class
+                 
+           ];
+        }
+    
+    
+        public function getTabs(): array{
+
+            return [
+  
+                    null=>Tab::make('All'),
+                    'new'=>Tab::make()->query(fn($query)=>$query->where('status','new')),
+                    'processing'=>Tab::make()->query(fn($query)=>$query->where('status','processing')),
+                    'shipped'=>Tab::make()->query(fn($query)=>$query->where('status','shipped')),
+                    'cancel'=>Tab::make()->query(fn($query)=>$query->where('status','canceled'))
+
+
+
+            ];
+  
+       }
+    
+    
+    }
+
+
+
+
+
--- app/Filament/Resources/OrderResource/Widgets/OrderStats.php
+++ app/Filament/Resources/OrderResource/Widgets/OrderStats.php
@@ -2,6 +2,7 @@
 
 namespace App\Filament\Resources\OrderResource\Widgets;
 
+use Filament\Forms\Components\Tabs\Tab;
 use Filament\Widgets\StatsOverviewWidget as BaseWidget;
 use Filament\Widgets\StatsOverviewWidget\Stat;
 
@@ -10,7 +11,11 @@ class OrderStats extends BaseWidget
     protected function getStats(): array
     {
         return [
-            //
+            //Stat::make('New Orders',Order::query()->where('status','new')->count())
         ];
     }
+
+   
+
+
 }
